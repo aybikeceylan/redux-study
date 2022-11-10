215d1b7c40baae83f5d247803eb2098059859cf5
aybike <aybikesayar@gmail.com>
2022-11-10 23:02:12 +0300

    first commit

---
+++ src/redux/index.jsx
@@ -0,0 +1,33 @@
+// const initialState = {
+//     counter: 0,
+// }
+
+// const reducer = (state, action) => {
+//     switch (action.type) {
+//         case "İNCREMENT":
+//             return { counter: state.counter + 1 }
+
+//         case "DECREMENT":
+
+//             return { counter: state.counter - 1 }
+//         case "CLEAR":
+
+//             return initialState
+
+//         default:
+//             return state
+//     }
+// }
+// export default reducer
+
+import { combineReducers, legacy_createStore as createStore } from "redux";
+import counterReducer from "./reducers/counterReducer";
+import todoReducer from "./reducers/todoReducer";
+
+const rootReducer = combineReducers({
+    count: counterReducer,
+    todo: todoReducer
+
+})
+
+export const store = createStore(rootReducer)
---
+++ src/redux/reducers/todoReducer.js
@@ -0,0 +1,19 @@
+import { ADD_TODO } from "../types/todoTypes";
+
+const initialState = {
+    todoList: [{ id: new Date().getTime(), text: "", completed: false }],
+};
+
+const todoReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case ADD_TODO:
+            return {
+                todoList: [...state.todoList,
+                { id: new Date().getTime(), text: payload, completed: false }]
+            };
+
+        default:
+            return state;
+    }
+};
+export default todoReducer;
